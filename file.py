37a58d327adf36502c64f8a58dd9e8c2935aee5a
techyguy951 <nellipudilokesh951@gmail.com>
2025-09-05 14:56:45 +0530

    Add simple OOP Car class code

--- file.py
+++ file.py
@@ -1,7 +1,20 @@
-import numpy as np
-x = np.array([1,2,3,4])
-print(x.dtype)
-new = np.float32(x)
-print(new.dtype)
-new_ = np.int_(new)
-print(new_.dtype)
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def display_info(self):
+        print(f"Car: {self.brand} {self.model}, Year: {self.year}")
+
+    def start_engine(self):
+        print(f"{self.brand} {self.model}'s engine started!")
+
+car1 = Car("Toyota", "Corolla", 2020)
+car2 = Car("Honda", "Civic", 2019)
+
+car1.display_info()
+car1.start_engine()
+
+car2.display_info()
+car2.start_engine()
