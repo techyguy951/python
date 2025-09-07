2cec4e6dc80cd4d2b0574b704f137122749260f4
techyguy951 <nellipudilokesh951@gmail.com>
2025-09-07 19:11:43 +0530

    Updated code

--- file.py
+++ file.py
@@ -1,20 +1,3 @@
-class Car:
-    def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
-
-    def display_info(self):
-        print(f"Car: {self.brand} {self.model}, Year: {self.year}")
-
-    def start_engine(self):
-        print(f"{self.brand} {self.model}'s engine started!")
-
-car1 = Car("Toyota", "Corolla", 2020)
-car2 = Car("Honda", "Civic", 2019)
-
-car1.display_info()
-car1.start_engine()
-
-car2.display_info()
-car2.start_engine()
+import numpy as np 
+x = np.array([1,2,3,4,5])
+print(np.min(x))
